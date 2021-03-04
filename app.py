94eb260c61f427c8d80ca07d0b2a2891054e2aae
1ntelligentmoron <74859246+1ntelligentmoron@users.noreply.github.com>
2021-03-04 19:31:48 +0800

    Added various features 4-Mar 19:30

---
+++ app.py
@@ -0,0 +1,10 @@
+import eel
+# import json
+import tkinter
+
+
+# initialise 127.0.0.1:8000 for eel
+eel.init('web')
+eel.start('zoom_id.html', mode='chrome')
+
+
---
+++ web/main.js
@@ -0,0 +1,18 @@
+// Communication with Python backend via Eel
+async function python() { // placeholder name
+    
+}
+
+
+// Frontend
+function onLoad() {
+
+    // Disable right click
+    // document.oncontextmenu = RightMouseDown;
+    // document.onmousedown = mouseDown; 
+    // function RightMouseDown() { return false; }
+    
+}
+
+function url(href) { window.open(href, '_self'); }
+function urlNewPage(href) { window.open(href); }
