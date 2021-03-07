b7308cd3025152d7589ea1273e095f34392fb17e
1ntelligentmoron <lokhayhl966@gmail.com>
2021-03-07 19:02:12 +0800

    Changed dir structure, began dev of setup page

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import eel
-# import json
-import tkinter
-import os
+import eel # framework
+# import json # pmi storage
+import os # get username: windows, unix only
+# import setup # local python file
 
 
 @eel.expose
 def username_to_js():
-    if os.getlogin != "null" and os.getlogin():
-        return "Welcome, {}!".format(os.getlogin())
-
+    suffix: str = ""
+    if os.getlogin():
+        suffix = ", {}".format(os.getlogin())
+    return "Welcome{}!".format(suffix)
 
 # initialise 127.0.0.1:8000 for eel
 eel.init('web')
